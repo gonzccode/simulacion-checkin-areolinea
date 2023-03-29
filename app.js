0a6d2d816b1a64490309fe0bd12fda561e406ba7
GonzaCC <acanazac@uni.pe>
2023-03-29 01:37:23 -0500

    Construyendo server

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("soy app")
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (request, response) => {
+    return response.json({
+        ok: true,
+        message: 'endpoint initial',
+        port: PORT
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Listen to PORT: ${PORT}`);
+});
